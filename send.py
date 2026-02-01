6565db8760c93f1a28a661af09c115f534576d32
SamuGavhane <samruddhi.gavhane@vaastav.tech>
2026-02-01 22:35:43 +0530

    deleted some lines if code

--- send.py
+++ send.py
@@ -7,13 +7,6 @@ def send_input():
         f = open("data.txt", "w")
         f.write(text)
         f.close()
-
-
-def send_input():
-    while True:
-        text = input("Sender: ")
-        if text == "":
-            break
         print("sent:", text)
 
 if __name__ == "__main__":
